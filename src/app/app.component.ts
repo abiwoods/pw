0e747230116a4caa56c4275b2718856ef1b116d8
Abi Bromley <abigailstephanie@hotmail.co.uk>
2020-02-17 12:01:17 +0000

    basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  password: string = '';
+  useLetters: boolean = false;
+  useNumbers: boolean = false;
+  useSymbols: boolean = false;
+  passwordLength: number = 0;
+
+  onButtonClick() {
+    const numbers = '0123456789';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.useLetters) {
+      validChars += letters;
+    }
+
+    if (this.useNumbers) {
+      validChars += numbers;
+    }
+
+    if (this.useSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPasword = '';
+
+    for (let i = 0; i < this.passwordLength; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPasword += validChars[index];
+    }
+
+    this.password = generatedPasword;
+  }
+
+  onChangeUseLetters() {
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onChangeLength(length: string) {
+    const parsedValue = parseInt(length);
+
+    if (!isNaN(parsedValue)){
+      this.passwordLength = parsedValue;
+    }
+  }
 }
